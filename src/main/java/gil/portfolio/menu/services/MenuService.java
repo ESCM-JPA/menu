3fdebd4fd52385939c60b4596a3a9177aab8d4d4
gil jea woo <kh6197@naver.com>
2024-08-23 13:08:43 +0900

    menu initial commit 2.

--- src/main/java/gil/portfolio/menu/services/MenuService.java
+++ src/main/java/gil/portfolio/menu/services/MenuService.java
@@ -2,7 +2,13 @@ package gil.portfolio.menu.services;
 
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class MenuService implements MenuRetrieveService {
 
+    @Override
+    public List retirieveMenu() {
+        return List.of();
+    }
 }
